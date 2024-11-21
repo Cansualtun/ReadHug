2b3b8bc6e5e377695b8168ca449bb12f11cbc8fb
halit uzan <halit.uzan@gmail.com>
2024-11-21 04:10:19 +0300

    formatDate added dateTime format and change toLocaleString

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,11 @@
-export function formatDate(date: string): string {
+export function formatDate(date: string, type: string = 'date'): string {
   if (!date) return '';
-  const formattedDate = new Date(date).toISOString().split('T')[0];
-
+  let formattedDate = '';
+  const newDate = new Date(date).toLocaleString().split(', ');
+  if (type == 'date') {
+    formattedDate = newDate[0];
+  } else if (type === 'dateTime') {
+    formattedDate = newDate[0] + ' ' + newDate[1].split(' AM')[0];
+  }
   return formattedDate;
 }
